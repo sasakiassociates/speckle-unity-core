097d43f4449ffa4b024f994adeb0d7c2bf9209b5
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-05-27 17:24:08 -0400

    Starting Node and Structure objects

--- Runtime/Ops/SpeckleNode.cs
+++ Runtime/Ops/SpeckleNode.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Speckle.ConnectorUnity.Mono;
 using Speckle.Core.Models;
@@ -6,6 +7,48 @@ using UnityEngine;
 
 namespace Speckle.ConnectorUnity.Ops
 {
+
+	/// <summary>
+	/// not really sure what this will be, but I think it would be helpful to have an object that mimics the container objects of speckle. 
+	/// </summary>
+	[Serializable]
+	internal class SpeckleLayer
+	{
+
+		[SerializeField] private string name;
+		[SerializeField] private Transform parent;
+		[SerializeField] private List<GameObject> data;
+
+		public SpeckleLayer()
+		{ }
+
+		public SpeckleLayer(string name)
+		{
+			this.name = name;
+			data = new List<GameObject>();
+		}
+
+		public void Parent(Transform t)
+		{
+			parent = t;
+		}
+
+		public void Add(object @object)
+		{ }
+
+	}
+
+	[Serializable]
+	internal class SpeckleStructure
+	{
+		public SpeckleStructure()
+		{
+			layers = new List<SpeckleLayer>();
+		}
+		
+		public List<SpeckleLayer> layers;
+
+	}
 
 	/// <summary>
 	/// A speckle node is pretty much the reference object that is first pulled from a commit
@@ -16,6 +59,8 @@ namespace Speckle.ConnectorUnity.Ops
 		[SerializeField] private string id;
 		[SerializeField] private string appId;
 		[SerializeField] private int childCount;
+
+		[SerializeField] private SpeckleStructure data;
 
 		/// <summary>
 		/// Reference object id
@@ -35,11 +80,41 @@ namespace Speckle.ConnectorUnity.Ops
 			_properties.Store(@base, props ?? excludedProps);
 		}
 
+		public void CreateHierarchy(Base @base)
+		{
+			foreach (var n in @base.GetDynamicMemberNames())
+				Debug.Log($"dynamic:{n}");
+
+			// TODO: check if there are layers or if this is just a single object
+
+			data = new SpeckleStructure();
+
+			// check if there are layers in the ref object
+			foreach (var member in @base.GetDynamicMembers())
+			{
+				Debug.Log($"dynamic member:{member}");
+				var layer = new SpeckleLayer(member);
+				layer.Add(@base[member]);
+				data.layers.Add(layer);
+			}
+
+			foreach (var n in @base.GetInstanceMembersNames())
+				Debug.Log($"instance:{n}");
+
+			foreach (var n in @base.GetInstanceMembers())
+				Debug.Log($"instance member:{n}");
+		}
+
 		public UniTask Set(Base @base)
 		{
 			id = @base.id;
 			appId = @base.applicationId;
 			childCount = (int)@base.totalChildrenCount;
+
+			CreateHierarchy(@base);
+
+			// this object will have all the objects within it's properties space
+			// let's take that structure and rebuild it 
 
 			return UniTask.CompletedTask;
 
