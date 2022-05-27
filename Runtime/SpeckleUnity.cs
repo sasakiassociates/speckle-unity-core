b51f9ff712c4140c0e0b8ab2070ae89a9d3dc1b8
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-05-27 17:23:39 -0400

    Moved editor function

--- Runtime/SpeckleUnity.cs
+++ Runtime/SpeckleUnity.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using Speckle.Core.Kits;
 using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace Speckle.ConnectorUnity
 {
@@ -28,5 +33,19 @@ namespace Speckle.ConnectorUnity
 				Debug.LogWarning(title + message);
 			}
 		}
+
+		#if UNITY_EDITOR
+		public static List<T> GetAllInstances<T>() where T : ScriptableObject
+		{
+			var guids = AssetDatabase.FindAssets("t:" + typeof(T).Name);
+			var items = new List<T>();
+			foreach (var g in guids)
+			{
+				var path = AssetDatabase.GUIDToAssetPath(g);
+				items.Add(AssetDatabase.LoadAssetAtPath<T>(path));
+			}
+			return items;
+		}
+		#endif
 	}
 }
