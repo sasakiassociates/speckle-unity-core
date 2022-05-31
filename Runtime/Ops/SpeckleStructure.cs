ba136483cdc2acff8d18ab7cfbba0547bf3a31e2
David Morgan <30870056+haitheredavid@users.noreply.github.com>
2022-05-31 14:04:22 -0500

    basic speckle tree, list, something structure for unity

---
+++ Runtime/Ops/SpeckleStructure.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Speckle.ConnectorUnity.Ops
+{
+	[Serializable]
+	public class SpeckleStructure
+	{
+		public SpeckleStructure()
+		{
+			layers = new List<SpeckleLayer>();
+			global = new SpeckleLayer();
+		}
+
+		public List<SpeckleLayer> layers;
+
+		public SpeckleLayer global;
+		
+		public void Add(SpeckleLayer layer)
+		{
+			layers ??= new List<SpeckleLayer>();
+			layers.Add(layer);
+		}
+	}
+}
